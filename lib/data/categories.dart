bdc4e747342c329c99c4c2aaf0ea672bf40dd96f
ntwari-egide <ntwariegide2@gmail.com>
2023-10-24 23:21:49 -0400

    feat: add dummy data

---
+++ lib/data/categories.dart
@@ -0,0 +1,18 @@
+// define a list of 20 food categories of US
+
+import 'package:flutter/material.dart';
+import 'package:hawks_nest_app/model/category.dart';
+
+const foodCategories = [
+  // define a list of 20 food categories of US
+  Category(id: 'c1', title: 'Italian', color: Colors.purple),
+  Category(id: 'c2', title: 'Quick & Easy', color: Colors.red),
+  Category(id: 'c3', title: 'Hamburgers', color: Colors.orange),
+  Category(id: 'c4', title: 'German', color: Colors.amber),
+  Category(id: 'c5', title: 'Light & Lovely', color: Colors.blue),
+  Category(id: 'c6', title: 'Exotic', color: Colors.green),
+  Category(id: 'c7', title: 'Breakfast', color: Colors.lightBlue),
+  Category(id: 'c8', title: 'Asian', color: Colors.lightGreen),
+  Category(id: 'c9', title: 'French', color: Colors.pink),
+  Category(id: 'c10', title: 'Summer', color: Colors.teal),
+];
