e97f1a1d19eefd6ecffa923ae3de23af30005d70
ntwari-egide <ntwariegide2@gmail.com>
2023-10-24 23:59:27 -0400

    feat: add splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_splash_screen/flutter_splash_screen.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hawks_nest_app/categories/CategoryScreen.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
-  colorScheme: ColorScheme.fromSeed(
-    brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 131, 57, 0),
-  ),
-  textTheme: GoogleFonts.latoTextTheme(),
+  textTheme: GoogleFonts.poppinsTextTheme(),
 );
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+  
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+
+  @override
+  void initialState(){
+    super.initState();
+    // add the splash screen here
+    hideSplashScreen();
+  }
+
+  // add the splash screen here
+  Future<void> hideSplashScreen() async {
+    await Future.delayed(const Duration(seconds: 9), () => {
+      FlutterSplashScreen.hide()
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
