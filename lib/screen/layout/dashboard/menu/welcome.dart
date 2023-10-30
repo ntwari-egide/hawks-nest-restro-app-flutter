0abcb6089a68fea85ba1dc9e48b0ac56d28301cf
ntwari-egide <ntwariegide2@gmail.com>
2023-10-29 23:17:50 -0400

    feat: implement rating

--- lib/screen/layout/dashboard/menu/welcome.dart
+++ lib/screen/layout/dashboard/menu/welcome.dart
@@ -62,87 +62,149 @@ class _MenuWelcomeScreenState extends State<MenuWelcomeScreen> {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Stack(children: [
-            Container(
-            height: 300,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                    'https://images.pexels.com/photos/3887985/pexels-photo-3887985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
-                fit: BoxFit.cover,
+          Stack(
+            children: [
+              Container(
+                height: 250,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://images.pexels.com/photos/3887985/pexels-photo-3887985.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                width: double.infinity,
+                // allow scrolling
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
               ),
-            ),
-            width: double.infinity,
-            // allow scrolling
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 100, left: 20, right: 20),
-            child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 40,
-                      width: 40,
-                      alignment: Alignment.center,
-                      decoration: const BoxDecoration(
-                        color: Colors.white,
-                        // rounded
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(100.0),
-                        ),
-                      ),
-                      child: const Icon(
-                        Icons.arrow_back_ios_new_sharp,
-                        color: Colors.black,
-                        size: 16,
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Container(
-                          height: 40,
-                          width: 40,
-                          alignment: Alignment.center,
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            // rounded
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(100.0),
+              Container(
+                padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            alignment: Alignment.center,
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              // rounded
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(100.0),
+                              ),
+                            ),
+                            child: const Icon(
+                              Icons.arrow_back_ios_new_sharp,
+                              color: Colors.black,
+                              size: 16,
                             ),
                           ),
-                          child: const Icon(
-                            Icons.share,
-                            color: Colors.black,
-                            size: 16,
-                          ),
-                        ),
-                        const SizedBox(width: 20),
-                        Container(
-                          height: 40,
-                          width: 40,
-                          alignment: Alignment.center,
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            // rounded
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(100.0),
+                          Row(
+                            children: [
+                              Container(
+                                height: 40,
+                                width: 40,
+                                alignment: Alignment.center,
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  // rounded
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(100.0),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.share,
+                                  color: Colors.black,
+                                  size: 16,
+                                ),
+                              ),
+                              const SizedBox(width: 20),
+                              Container(
+                                height: 40,
+                                width: 40,
+                                alignment: Alignment.center,
+                                decoration: const BoxDecoration(
+                                  color: Colors.white,
+                                  // rounded
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(100.0),
+                                  ),
+                                ),
+                                child: const Icon(
+                                  Icons.favorite_outline,
+                                  color: Colors.black,
+                                  size: 16,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Stack(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(top: 18),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 12),
+                            height: 200,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                              // add border and shodown bottom
+                              boxShadow: const [
+                                BoxShadow(
+                                  color: Colors.white,
+                                  spreadRadius: 1,
+                                  blurRadius: 1,
+                                  offset: Offset(0, 1),
+                                ),
+                              ],
+                              border: Border.all(
+                                color: const Color.fromARGB(14, 0, 0, 0),
+                                width: 1,
+                              ),
                             ),
                           ),
-                          child: const Icon(
-                            Icons.favorite_outline,
-                            color: Colors.black,
-                            size: 16,
+                          Center(
+                            child: Container(
+                              width: 180,
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 8),
+                              decoration: const BoxDecoration(
+                                color: Colors.orange,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(100.0),
+                                ),
+                              ),
+                              child: const Center(
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.white,
+                                      size: 20,
+                                    ),
+                                    Text(
+                                      '4.8(1k+ Reviews)',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-          ),
-          ],)
+            ],
+          )
         ],
       ),
     );
